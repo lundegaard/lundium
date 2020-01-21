41c6110402fa3879f6448edf4dc76e469a6275f0
Martin Macháček <martin.machacek@lundegaard.eu>
2020-01-21 09:54:55 +0100

    Add locale support with basic texts

---
+++ packages/locale/src/en_US.js
@@ -0,0 +1,9 @@
+const en_US = {
+	clearText: 'Clear',
+	closeText: 'Close',
+	loadingText: 'Loading…',
+	noOptionsText: 'No options',
+	openText: 'Open',
+};
+
+export default en_US;
---
+++ packages/locale/webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const glob = require('glob');
+
+const filePaths = glob
+	.sync('**/*.js', { cwd: './src' })
+	.map(fileName => fileName.replace('.js', ''));
+
+const entries = filePaths.reduce(
+	(entries, newEntry) => ({
+		...entries,
+		[newEntry]: `./src/${newEntry}.js`,
+	}),
+	{}
+);
+
+module.exports = {
+	mode: 'production',
+	entry: entries,
+	output: {
+		filename: '[name].js',
+		path: path.resolve('./dist'),
+		libraryTarget: 'umd',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				options: { rootMode: 'upward' },
+			},
+		],
+	},
+	// In case of troubleshooting: https://github.com/lerna/lerna/issues/1049
+	externals: ['react', 'react-dom'],
+};
