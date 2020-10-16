d86cb7e45123cdeb75c8c4b6e356ead8c18a272d
veronika.wernerova <veronika.wernerova@lundegaard.eu>
2020-10-16 12:59:56 +0200

    #39 add disabled prop to link

--- apps/storybook/src/lundium/components/Link.stories.js
+++ apps/storybook/src/lundium/components/Link.stories.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'lundium';
+import { boolean } from '@storybook/addon-knobs';
 
 export default { title: 'Link', component: Link };
 
 export const basic = () => (
-	<Link href="http://github.com/tommmyy/ramda-extension">Ramda Extension</Link>
+	<Link href="http://github.com/tommmyy/ramda-extension" disabled={boolean('disabled', false)}>
+		Ramda Extension
+	</Link>
 );
